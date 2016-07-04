8b3362291a18a31a65c5f37b26ec1a70ca9d45ae
Prayash Mohapatra <catchprayash@gmail.com>
2016-07-04 20:32:42 +0530

    Allow all available encodings. Select default encoding.

--- src/window.js
+++ src/window.js
@@ -717,28 +717,52 @@
 
     Sao.Window.CSV = Sao.class_(Object, {
         init: function(){
-            // Not in Javascript FileReader#readAsText:
-            // ["cp037", "cp424", "cp437", "cp500", "cp720", "cp737", 
-            // "cp775", "cp850", "cp852", "cp855", "cp856", "cp857", "cp858",
-            // "cp860", "cp861", "cp862", "cp863", "cp864", "cp865", "cp869", 
-            // "cp874", "cp875",  "cp932", "cp949", "cp950", "cp1006", "cp1026",
-            // "cp1140", "euc_jis_2004", "euc_jisx0213", "hz", "iso2022_jp_1",
-            // "iso2022_jp_2", "iso2022_jp_2004", "iso2022_jp_3", "iso2022_jp_ext"
-            // "iso8859_16", "johab", "mac_greek", "mac_iceland", "mac_latin2",
-            // "mac_turkish", "ptcp154", "shift_jis_2004", "shift_jisx0213",
-            // "utf_32", "utf_32_be", "utf_32_le", "utf_7","utf_8_sig"];
-
-            // "mac_cyrillic" <== "x-mac-cyrillic"
-            // "mac_roman" <== "x-mac-roman"
-
-            this.encodings = ["ascii", "big5", "big5-hkscs", "cp866", "cp1250",
-            "cp1251", "cp1252", "cp1253", "cp1254", "cp1255", "cp1256", 
-            "cp1257", "cp1258", "euc-jp", "euc-kr", "gb2312", "gbk", "gb18030",
-            "iso2022-jp", "iso2022-kr", "latin1", "iso8859-2", "iso8859-3",
-            "iso8859-4", "iso8859-5", "iso8859-6", "iso8859-7", "iso8859-8",
-            "iso8859-9", "iso8859-10", "iso8859-13", "iso8859-14", "iso8859-15",
-            "koi8_r", "koi8-u", "shift_jis", "utf-16", "utf-16be", "utf-16le",
-            "utf-8"];
+            this.encodings = ["866", "ansi_x3.4-1968", "arabic", "ascii",
+            "asmo-708", "big5", "big5-hkscs", "chinese", "cn-big5", "cp1250",
+            "cp1251", "cp1252", "cp1253", "cp1254", "cp1255", "cp1256",
+            "cp1257", "cp1258", "cp819", "cp866", "csbig5", "cseuckr",
+            "cseucpkdfmtjapanese", "csgb2312", "csibm866", "csiso2022jp",
+            "csiso2022kr", "csiso58gb231280", "csiso88596e", "csiso88596i",
+            "csiso88598e", "csiso88598i", "csisolatin1", "csisolatin2",
+            "csisolatin3", "csisolatin4", "csisolatin5", "csisolatin6",
+            "csisolatin9", "csisolatinarabic", "csisolatincyrillic",
+            "csisolatingreek", "csisolatinhebrew", "cskoi8r", "csksc56011987",
+            "csmacintosh", "csshiftjis", "cyrillic", "dos-874", "ecma-114",
+            "ecma-118", "elot_928", "euc-jp", "euc-kr", "gb18030", "gb2312",
+            "gb_2312", "gb_2312-80", "gbk", "greek", "greek8", "hebrew",
+            "hz-gb-2312", "ibm819", "ibm866", "iso-2022-cn", "iso-2022-cn-ext",
+            "iso-2022-jp", "iso-2022-kr", "iso-8859-1", "iso-8859-10",
+            "iso-8859-11", "iso-8859-13", "iso-8859-14", "iso-8859-15",
+            "iso-8859-16", "iso-8859-2", "iso-8859-3", "iso-8859-4",
+            "iso-8859-5", "iso-8859-6", "iso-8859-6-e", "iso-8859-6-i",
+            "iso-8859-7", "iso-8859-8", "iso-8859-8-e", "iso-8859-8-i",
+            "iso-8859-9", "iso-ir-100", "iso-ir-101", "iso-ir-109",
+            "iso-ir-110", "iso-ir-126", "iso-ir-127", "iso-ir-138",
+            "iso-ir-144", "iso-ir-148", "iso-ir-149", "iso-ir-157", "iso-ir-58",
+            "iso8859-1", "iso8859-10", "iso8859-11", "iso8859-13", "iso8859-14",
+            "iso8859-15", "iso8859-2", "iso8859-3", "iso8859-4", "iso8859-5",
+            "iso8859-6", "iso8859-7", "iso8859-8", "iso8859-9", "iso88591",
+            "iso885910", "iso885911", "iso885913", "iso885914", "iso885915",
+            "iso88592", "iso88593", "iso88594", "iso88595", "iso88596",
+            "iso88597", "iso88598", "iso88599", "iso_8859-1", "iso_8859-15",
+            "iso_8859-1:1987", "iso_8859-2", "iso_8859-2:1987", "iso_8859-3",
+            "iso_8859-3:1988", "iso_8859-4", "iso_8859-4:1988", "iso_8859-5",
+            "iso_8859-5:1988", "iso_8859-6", "iso_8859-6:1987", "iso_8859-7",
+            "iso_8859-7:1987", "iso_8859-8", "iso_8859-8:1988", "iso_8859-9",
+            "iso_8859-9:1989", "koi", "koi8", "koi8-r", "koi8-ru", "koi8-u",
+            "koi8_r", "korean", "ks_c_5601-1987", "ks_c_5601-1989", "ksc5601",
+            "ksc_5601", "l1", "l2", "l3", "l4", "l5", "l6", "l9", "latin1",
+            "latin2", "latin3", "latin4", "latin5", "latin6", "logical", "mac",
+            "macintosh", "ms932", "ms_kanji", "shift-jis", "shift_jis", "sjis",
+            "sun_eu_greek", "tis-620", "unicode-1-1-utf-8", "us-ascii",
+            "utf-16", "utf-16be", "utf-16le", "utf-8", "utf8", "visual",
+            "windows-1250", "windows-1251", "windows-1252", "windows-1253",
+            "windows-1254", "windows-1255", "windows-1256", "windows-1257",
+            "windows-1258", "windows-31j", "windows-874", "windows-949",
+            "x-cp1250", "x-cp1251", "x-cp1252", "x-cp1253", "x-cp1254",
+            "x-cp1255", "x-cp1256", "x-cp1257", "x-cp1258", "x-euc-jp", "x-gbk",
+            "x-mac-cyrillic", "x-mac-roman", "x-mac-ukrainian", "x-sjis",
+            "x-user-defined", "x-x-big5"];
         }
     });
 
@@ -970,7 +994,12 @@
                 }).html(this.encodings[i]).appendTo(this.el_csv_encoding);
             }
 
-            this.el_csv_encoding.children('option[value="utf-8"]')
+            var enc = 'utf-8';
+            if (navigator.platform == 'Win32' ||
+                navigator.platform == 'Windows') {
+                enc = 'cp1252';
+            }
+            this.el_csv_encoding.children('option[value="'+enc+'"]')
             .attr('selected', 'selected');
 
             var div_encoding = jQuery('<div/>', {
